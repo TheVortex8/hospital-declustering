d26c028259604447e3e09c211ae0ea0521f69e73
Amine <mohamed-amine.elarabi.1@ens.etsmtl.ca>
2025-01-25 14:51:23 -0500

    added queue implemenattion

--- netlify/functions/queue.ts
+++ netlify/functions/queue.ts
@@ -1,11 +1,12 @@
 import { Handler } from '@netlify/functions';
+import { getQueue } from './mchacks-routes';
 
 const handler: Handler = async (event) => {
-  const sort = event?.queryStringParameters['sort'];
+  const sort = event?.queryStringParameters?.['sort'] ||  'arrival_time';
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: sort }),
+    body: JSON.stringify({ message: getQueue(sort) }),
   };
 };
 
