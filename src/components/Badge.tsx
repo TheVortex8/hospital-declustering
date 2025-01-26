b3c12a9413317daf6b8f3b068148014a0b7e6997
Abdul Rahman Zahid <103886116+LLAbdul@users.noreply.github.com>
2025-01-26 00:18:18 -0500

    admin dashboard db addon

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import "../styles/Badge.css";
 
 interface BadgeProps {
-    color: string;
-    text: string;
+    color: { border: string };
+    label: string;
 }
 
-export const Badge: React.FC<BadgeProps> = ({ color, text }) => {
+export const Badge: React.FC<BadgeProps> = ({ color: style, label }) => {
     return (
-        <span className="badge" style={{ borderColor: color }}>
-            <span className="dot" style={{ backgroundColor: color }}></span>
-            {text}
+        <span className="badge" style={{ borderColor: style.border }}>
+            <span className="dot" style={{ backgroundColor: style.border }}></span>
+            {label}
         </span>
     );
 };
--- src/pages/adminPage.tsx
+++ src/pages/adminPage.tsx
@@ -1,17 +1,44 @@
 import { useEffect, useState } from "react";
 import "../styles/AdminPage.css";
-import Badge from "../components/Toast"; // Import the Badge component
-import { PatientPhase, PatientsQueue } from "../../types/patient";
+import { Badge } from "../components/Badge"; // Import the Badge component
+import { PatientPhase, PatientsQueue, TriageCategory } from "../../types/patient";
+import React from "react";
 
 export function AdminPage() {
   const [queue, setQueue] = useState<PatientsQueue>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [tiragedPatientsList , setTiragedPatientList] = React.useState(false);
+  const [treatmentPatientList, setTreatmentPatientList] = React.useState(false);
+  const [admittedPatientList, setAdmittedPatientList] = React.useState(false);
+const [pendingInvestigationsList, setPendingInvestigationsList] = React.useState(false);
+    const [dischargedPatientList, setDischargedPatientList] = React.useState(false);
+    const [registeredPatientList, setRegisteredPatientList] = React.useState(false);
+  
+  const triagedPatients = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.TRIAGED);
+
+const treatmentPatients = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.TREATMENT);
+
+  const admittedPatients = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.ADMITTED);
+
+const pendingInvestigations = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.INVESTIGATIONS_PENDING);
+
+const dischargedPatients = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.DISCHARGED);
+
+  const registeredPatients = (queue?.patients || [])
+  .filter(patient => patient.status.current_phase === PatientPhase.DISCHARGED);
+
 
   useEffect(() => {
+    
     const fetchPatients = async () => {
       try {
-        const response = await fetch("http://localhost:8888/api/get");
+        const response = await fetch("http://localhost:8888/api/get")
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -44,6 +71,7 @@ export function AdminPage() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
+  const triage = ["Resuscitation", "Emergent", "Urgent", "Less Urgent", "Non Urgent"];
 
   return (
     <div>
@@ -53,12 +81,11 @@ export function AdminPage() {
         <div>
           <h3>Triage</h3>
           <ul className="kanban-column">
-            {queue.patients && queue.patients
-              .filter(patient => patient.status.current_phase === PatientPhase.TRIAGED)
+            {triagedPatients
               .map((patient) => (
                 <li className="kanban-item" key={patient.name}>
                   {patient.name}
-                  <Badge label={patient.status.current_phase} style={getBadgeStyle(patient.status)} />
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
                 </li>
               ))}
           </ul>
@@ -68,12 +95,13 @@ export function AdminPage() {
         <div>
           <h3>Admitted</h3>
           <ul className="kanban-column">
-            {queue.admitted && queue.admitted.map((patient) => (
-              <li className="kanban-item" key={patient.name}>
-                {patient.name}
-                <Badge label={patient.status} style={getBadgeStyle(patient.status)} />
-              </li>
-            ))}
+            {
+             admittedPatients .map((patient) => (
+                <li className="kanban-item" key={patient.name}>
+                  {patient.name}
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
+                </li>
+              ))}
           </ul>
         </div>
 
@@ -81,12 +109,13 @@ export function AdminPage() {
         <div>
           <h3>Treatment</h3>
           <ul className="kanban-column">
-            {queue.treatment && queue.treatment.map((patient) => (
-              <li className="kanban-item" key={patient.name}>
-                {patient.name}
-                <Badge label={patient.status} style={getBadgeStyle(patient.status)} />
-              </li>
-            ))}
+            {treatmentPatients
+              .map((patient) => (
+                <li className="kanban-item" key={patient.name}>
+                  {patient.name}
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
+                </li>
+              ))}
           </ul>
         </div>
 
@@ -94,12 +123,13 @@ export function AdminPage() {
         <div>
           <h3>Pending Investigations</h3>
           <ul className="kanban-column">
-            {queue.pendingInvestigations && queue.pendingInvestigations.map((patient) => (
-              <li className="kanban-item" key={patient.name}>
-                {patient.name}
-                <Badge label={patient.status} style={getBadgeStyle(patient.status)} />
-              </li>
-            ))}
+            {pendingInvestigations
+              .map((patient) => (
+                <li className="kanban-item" key={patient.name}>
+                  {patient.name}
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
+                </li>
+              ))}
           </ul>
         </div>
 
@@ -107,12 +137,13 @@ export function AdminPage() {
         <div>
           <h3>Discharged</h3>
           <ul className="kanban-column">
-            {queue.discharged && queue.discharged.map((patient) => (
-              <li className="kanban-item" key={patient.name}>
-                {patient.name}
-                <Badge label={patient.status} style={getBadgeStyle(patient.status)} />
-              </li>
-            ))}
+            {dischargedPatients
+              .map((patient) => (
+                <li className="kanban-item" key={patient.name}>
+                  {patient.name}
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
+                </li>
+              ))}
           </ul>
         </div>
 
@@ -120,15 +151,18 @@ export function AdminPage() {
         <div>
           <h3>Registered</h3>
           <ul className="kanban-column">
-            {queue.registered && queue.registered.map((patient) => (
-              <li className="kanban-item" key={patient.name}>
-                {patient.name}
-                <Badge label={patient.status} style={getBadgeStyle(patient.status)} />
-              </li>
-            ))}
+            {registeredPatients
+              .map((patient) => (
+                <li className="kanban-item" key={patient.name}>
+                  {patient.name}
+                  <Badge label={triage[patient.triageCategory - 1]} color={getBadgeStyle(patient.status)} />
+                </li>
+              ))}
           </ul>
         </div>
       </div>
     </div>
   );
 }
+
+
